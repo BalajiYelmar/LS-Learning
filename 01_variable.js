612ad41283dadb29ced183a71e275c8b0965c722
BalajiYelmar <balaji.yelmar@gmail.com>
2023-09-20 22:35:24 +0530

    Variable declaration and initialization
    Variable declaration and initialization

---
+++ 01_variable.js
@@ -0,0 +1,26 @@
+var firstName; // variable declaration
+firstName = "Balaji";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Balaji"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Yelmar"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "5.7"; // initialization
+console.log(height);
+height = "6.8 feet";// update
+console.log(height);
+
+
+var num1=100;
+var num2=200;
+var temp;
+temp=num2;
+num1=temp;
+num2=num1;
+console.log("num1="+num1);
+console.log("num2="+num2);
---
+++ 01_variableAssignment.js
@@ -0,0 +1,30 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Balaji Yelmar";
+console.log("Name :"+myFullName);
+myFullName="Balaji Sukhadev Yelmar";
+console.log("Full Name :"+myFullName);
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+var firstName="Balaji";
+console.log("FirstName :"+firstName);
+
+var lastNameName="Yelmar";
+console.log("LastName :"+lastNameName);
+
+var age=33;
+console.log("Age :"+age);
+
+var address="Punawale";
+console.log("Address :"+address);
+
+var pinCode=411033;
+console.log("PinCode :"+pinCode);
+
+var gender="Male";
+console.log("Gender :"+gender);
+
+var isMarried="Yes";
+console.log("Married :"+isMarried);
+
