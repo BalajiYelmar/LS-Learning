949557f748877856736a5569e84f02e3c4e04901
BalajiYelmar <balaji.yelmar@gmail.com>
2023-09-22 21:16:45 +0530

    DataType Assignment done
    DataType Assignment done

---
+++ 02_datatypeAssignment01.js
@@ -0,0 +1,35 @@
+var firstName="Balaji";
+var middleName="Sukhadev";
+var lastName="Yelmar";
+
+var mobileNumber=9561618123;
+var pinCode=413304;
+var bikeNo=2417;
+
+var isMarried=true;
+var isFourWheeler=false;
+var isHandicap=false;
+
+console.log("====================Assignment 1===================");
+console.log("====================Step 1===================");
+console.log("====================String DataType=========================");
+
+console.log("First Name :"+firstName,"Datatype :"+typeof firstName);
+console.log("Middle Name :"+middleName,"Datatype :"+typeof middleName);
+console.log("Last Name :"+lastName,"Datatype :"+typeof lastName);
+
+console.log("====================Number Datatype=========================");
+console.log("Mobile Number :"+mobileNumber,"Datatype :"+typeof mobileNumber);
+console.log("Pin Code :"+pinCode,"Datatype :"+typeof pinCode);
+console.log("Bike No. :"+bikeNo,"Datatype :"+typeof bikeNo);
+
+console.log("====================Boolean Datatype=========================");
+console.log("Married :"+isMarried,"Datatype :"+typeof isMarried);
+console.log("Four Wheeler :"+isFourWheeler,"Datatype :"+typeof isFourWheeler);
+console.log("Handicap :"+isHandicap,"Datatype :"+typeof isHandicap);
+
+console.log("====================Step 2===================");
+var isNull;
+console.log(isNull+" Variable has been declared but has yet not been assigned a value");
+var isNull=null;
+console.log(isNull+ " It can be assigned to a variable  as representation of no value");
